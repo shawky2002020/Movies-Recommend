d8c905ac795e629127ee7452b83b2adae7cb7a25
Tadros-wagih <133041939+Tadros-wagih@users.noreply.github.com>
2025-05-10 23:54:29 +0300

    Update main.java

--- src/classes/run.java
+++ src/classes/run.java
@@ -1,13 +1,35 @@
-package classes;
+package org.example;
 
-public class run {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Validation validation = new Validation();
+        FileHandler fileHandler = new FileHandler(validation);
+
+        List<Movie> movies = null;
+        List<User> users = null;
         try {
-            // Generate recommendations and write to file
-            MovieRecommendationSystem.generateRecommendations("recommendations.txt","movies_test.txt","users_test.txt");
-
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+            users = fileHandler.loadUsers("users.txt");
+            movies = fileHandler.loadMovies("movies.txt");
+        } catch (InputException e) {
+            System.out.println(e.getMessage());
+            fileHandler.writeOutput("recommendations.txt", null, e.getMessage());
+            return;
         }
+
+        if (movies == null || users == null) return;
+
+        RecommendationFinder recommendationFinder = new RecommendationFinder(movies);
+        List<Recommendation> recommendations = new ArrayList<>();
+
+        for (User user: users) {
+            List<String> movieTitles = recommendationFinder.recommend(user);
+            recommendations.add(new Recommendation(user.getUserID(), user.getUsername(), movieTitles));
+        }
+
+        fileHandler.writeOutput("recommendations.txt", recommendations, null);
     }
 }
